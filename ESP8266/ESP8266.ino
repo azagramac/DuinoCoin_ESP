e2d1fabb1921415da2fcf7fada0598f63ef51a6a
Jose l. Azagra <azagramac@gmail.com>
2023-04-26 13:52:04 +0200

    v3.4

--- ESP8266/ESP8266.ino
+++ ESP8266/ESP8266.ino
@@ -1,9 +1,9 @@
 /*
-   ____  __  __  ____  _  _  _____       ___  _____  ____  _  _ 
+   ____  __  __  ____  _  _  _____       ___  _____  ____  _  _
   (  _ \(  )(  )(_  _)( \( )(  _  )___  / __)(  _  )(_  _)( \( )
-   )(_) ))(__)(  _)(_  )  (  )(_)((___)( (__  )(_)(  _)(_  )  ( 
+   )(_) ))(__)(  _)(_  )  (  )(_)((___)( (__  )(_)(  _)(_  )  (
   (____/(______)(____)(_)\_)(_____)     \___)(_____)(____)(_)\_)
-  Official code for ESP8266 boards                   version 3.1
+  Official code for ESP8266 boards                   version 3.4
 
   Duino-Coin Team & Community 2019-2022 © MIT Licensed
   https://duinocoin.com
@@ -14,16 +14,16 @@
 */
 
 /* If optimizations cause problems, change them to -O0 (the default)
-NOTE: For even better optimizations also edit your Crypto.h file.
-On linux that file can be found in the following location:
-~/.arduino15//packages/esp8266/hardware/esp8266/3.0.2/cores/esp8266/ */
+  NOTE: For even better optimizations also edit your Crypto.h file.
+  On linux that file can be found in the following location:
+  ~/.arduino15//packages/esp8266/hardware/esp8266/3.0.2/cores/esp8266/ */
 #pragma GCC optimize ("-Ofast")
 
 /* If during compilation the line below causes a
   "fatal error: arduinoJson.h: No such file or directory"
   message to occur; it means that you do NOT have the
-  ArduinoJSON library installed. To install it, 
-  go to the below link and follow the instructions: 
+  ArduinoJSON library installed. To install it,
+  go to the below link and follow the instructions:
   https://github.com/revoxhere/duino-coin/issues/832 */
 #include <ArduinoJson.h>
 
@@ -35,7 +35,7 @@ On linux that file can be found in the following location:
   follow the instructions of the readme file:
   https://github.com/esp8266/Arduino */
 #include <bearssl/bearssl.h>
-#include <TypeConversion.h>
+//#include <TypeConversion.h>
 
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
@@ -46,29 +46,117 @@ On linux that file can be found in the following location:
 #include <Ticker.h>
 #include <ESP8266WebServer.h>
 
+// Uncomment the line below if you wish to use a DHT sensor (Duino IoT beta)
+// #define USE_DHT
+
+// Uncomment the line below if you wish to register for IOT updates with an MQTT broker
+// #define USE_MQTT
+
+// If you don't know what MQTT means check this link:
+// https://www.techtarget.com/iotagenda/definition/MQTT-MQ-Telemetry-Transport
+
+#ifdef USE_DHT
+float temp = 0.0;
+float hum = 0.0;
+
+// Install "DHT sensor library" if you get an error
+#include <DHT.h>
+// Change D3 to the pin you've connected your sensor to
+#define DHTPIN D3
+// Set DHT11 or DHT22 accordingly
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+#endif
+
+#ifdef USE_MQTT
+// Install "PubSubClient" if you get an error
+#include <PubSubClient.h>
+
+long lastMsg = 0;
+
+// Change the part in brackets to your MQTT broker address
+#define mqtt_server "broker.hivemq.com"
+// broker.hivemq.com is for testing purposes, change it to your broker address
+
+// Change this to your MQTT broker port
+#define mqtt_port 1883
+// If you want to use user and password for your MQTT broker, uncomment the line below
+// #define mqtt_use_credentials
+
+// Change the part in brackets to your MQTT broker username
+#define mqtt_user "My cool mqtt username"
+// Change the part in brackets to your MQTT broker password
+#define mqtt_password "My secret mqtt pass"
+
+// Change this if you want to send data to the topic every X milliseconds
+#define mqtt_update_time 5000
+
+// Change the part in brackets to your MQTT humidity topic
+#define humidity_topic "sensor/humidity"
+// Change the part in brackets to your MQTT temperature topic
+#define temperature_topic "sensor/temperature"
+
+WiFiClient espClient;
+PubSubClient mqttClient(espClient);
+
+void mqttReconnect()
+{
+  // Loop until we're reconnected
+  while (!mqttClient.connected())
+  {
+    Serial.print("Attempting MQTT connection...");
+
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+
+    // Attempt to connect
+#ifdef mqtt_use_credentials
+    if (mqttClient.connect("ESP8266Client", mqtt_user, mqtt_password))
+#else
+    if (mqttClient.connect(clientId.c_str()))
+#endif
+    {
+      Serial.println("connected");
+    }
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.print(mqttClient.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+#endif
+
+// IP Static
 IPAddress local_IP(192, 168, 1, 10);
 IPAddress gateway(192, 168, 1, 1);
 IPAddress subnet(255, 255, 255, 0);
 IPAddress primaryDNS(1, 1, 1, 1);
 IPAddress secondaryDNS(1, 0, 0, 1);
 
-namespace {
-const char* SSID = "YOUR_WIFI";              // Change the part in brackets to your WiFi name
-const char* PASSWORD = "YOUR_PASS_WIFI";     // Change the part in brackets to your WiFi password
-const char* USERNAME = "YOUR_USERNAME";      // Change the part in brackets to your Duino-Coin username
-const char* RIG_IDENTIFIER = "ESP8266";      // Change the part in brackets if you want to set a custom miner name (use Auto to autogenerate)
-const char* MINER_KEY = "YOUR_KEY_WALLET";   // Change the part in brackets to your mining key (if you enabled it in the wallet)
-const bool USE_HIGHER_DIFF = true;           // Change false to true if using 160 MHz clock mode to not get the first share rejected
-const bool WEB_HASH_UPDATER = true;          // Change false to true if you want to update hashrate in browser without reloading page
-const bool LED_BLINKING = true;              // Change true to false if you want to disable led blinking(But the LED will work in the beginning until esp connects to the pool)
-
+namespace
+{
+const char *DUCO_USER = "YOUR_USERNAME_WALLET";    // Change the part in brackets to your Duino-Coin username
+const char *MINER_KEY = "YOUR_KEY_WALLET";         // Change the part in brackets to your mining key (if you have enabled it in the wallet)
+const char* SSID = "YOYOUR_SSID_WIFI";             // Change the part in brackets to your WiFi name
+const char *PASSWORD = "YOUR_PASS_WIFI";           // Change the part in brackets to your WiFi password
+const char *RIG_IDENTIFIER = "ESP8266-1";          // Change the part in brackets if you want to set a custom miner name (use Auto to autogenerate, None for no name)
+const bool USE_HIGHER_DIFF = true;                 // Set to true to use the 160 MHz overclock mode (and not get the first share rejected)
+const bool WEB_DASHBOARD = true;                   // Set to true if you want to host the dashboard page (available on ESPs IP address)
+const bool WEB_HASH_UPDATER = false;               // Set to true if you want to update hashrate in browser without reloading the page
+const bool LED_BLINKING = true;                    // Set to false if you want to disable the onboard led blinking when finding shares
 
 /* Do not change the lines below. These lines are static and dynamic variables
    that will be used by the program for counters and measurements. */
 const char * DEVICE = "ESP8266";
 const char * POOLPICKER_URL[] = {"https://server.duinocoin.com/getPool"};
 const char * MINER_BANNER = "Official ESP8266 Miner";
-const char * MINER_VER = "3.1";
+const char * MINER_VER = "3.4";
 unsigned int share_count = 0;
 unsigned int port = 0;
 unsigned int difficulty = 0;
@@ -87,7 +175,6 @@ const char WEBSITE[] PROGMEM = R"=====(
     https://github.com/revoxhere/duino-coin
     https://duinocoin.com
 -->
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -96,7 +183,6 @@ const char WEBSITE[] PROGMEM = R"=====(
     <link rel="shortcut icon" href="https://github.com/revoxhere/duino-coin/blob/master/Resources/duco.png?raw=true">
     <link rel="icon" type="image/png" href="https://github.com/revoxhere/duino-coin/blob/master/Resources/duco.png?raw=true">
 </head>
-
 <body>
     <section class="section">
         <div class="container">
@@ -190,6 +276,26 @@ const char WEBSITE[] PROGMEM = R"=====(
                                     Miner version
                                 </div>
                             </div>
+)====="
+#ifdef USE_DHT
+"                            <div class=\"column\" style=\"min-width:15em\">"
+"                                <div class=\"title is-size-5 mb-0\">"
+"                                    @@TEMP@@ °C"
+"                                </div>"
+"                                <div class=\"heading is-size-5\">"
+"                                    Temperature"
+"                                </div>"
+"                            </div>"
+"                            <div class=\"column\" style=\"min-width:15em\">"
+"                                <div class=\"title is-size-5 mb-0\">"
+"                                    @@HUM@@ %"
+"                                </div>"
+"                                <div class=\"heading is-size-5\">"
+"                                    Humidity"
+"                                </div>"
+"                            </div>"
+#endif
+  R"=====(
                         </div>
                     </div>
                 </div>
@@ -230,14 +336,13 @@ const char WEBSITE[] PROGMEM = R"=====(
         </script>
     </section>
 </body>
-
 </html>
 )=====";
 
 ESP8266WebServer server(80);
 
 void hashupdater(){ //update hashrate every 3 sec in browser without reloading page
-  server.send(200, "text/plane", String(hashrate / 1000));
+  server.send(200, "text/plain", String(hashrate / 1000));
   Serial.println("Update hashrate on page");
 };
 
@@ -306,7 +411,7 @@ String START_DIFF = "";
 // Loop WDT... please don't feed me...
 // See lwdtcb() and lwdtFeed() below
 Ticker lwdTimer;
-#define LWD_TIMEOUT   60000
+#define LWD_TIMEOUT   20000
 
 unsigned long lwdCurrentMillis = 0;
 unsigned long lwdTimeOutMillis = LWD_TIMEOUT;
@@ -316,7 +421,6 @@ unsigned long lwdTimeOutMillis = LWD_TIMEOUT;
 
 #define LED_BUILTIN 2
 
-#define BLINK_SHARE_FOUND    1
 #define BLINK_SETUP_COMPLETE 2
 #define BLINK_CLIENT_CONNECT 3
 #define BLINK_RESET_DEVICE   5
@@ -326,7 +430,7 @@ void SetupWifi() {
   if(!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
     Serial.println(F("Error config Network..."));
   }
-  
+
   Serial.println("Connecting to: " + String(SSID));
   WiFi.mode(WIFI_STA); // Setup ESP in client mode
   WiFi.setSleepMode(WIFI_NONE_SLEEP);
@@ -382,6 +486,8 @@ void blink(uint8_t count, uint8_t pin = LED_BUILTIN) {
       digitalWrite(pin, state ^= HIGH);
       delay(50);
     }
+  } else {
+    digitalWrite(LED_BUILTIN, HIGH);
   }
 }
 
@@ -415,23 +521,6 @@ void handleSystemEvents(void) {
   yield();
 }
 
-// https://stackoverflow.com/questions/9072320/split-string-into-string-array
-String getValue(String data, char separator, int index)
-{
-  int found = 0;
-  int strIndex[] = {0, -1};
-  int max_index = data.length() - 1;
-
-  for (int i = 0; i <= max_index && found <= index; i++) {
-    if (data.charAt(i) == separator || i == max_index) {
-      found++;
-      strIndex[0] = strIndex[1] + 1;
-      strIndex[1] = (i == max_index) ? i + 1 : i;
-    }
-  }
-  return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
-}
-
 void waitForClientData(void) {
   client_buffer = "";
 
@@ -452,7 +541,7 @@ void ConnectToServer() {
     return;
 
   Serial.println("\n\nConnecting to the Duino-Coin server...");
-  while (!client.connect(host, port));
+  while (!client.connect(host.c_str(), port));
 
   waitForClientData();
   Serial.println("Connected to the server. Server version: " + client_buffer );
@@ -484,20 +573,76 @@ void dashboard() {
   s.replace("@@ID@@", String(RIG_IDENTIFIER));
   s.replace("@@MEMORY@@", String(ESP.getFreeHeap()));
   s.replace("@@VERSION@@", String(MINER_VER));
-
+#ifdef USE_DHT
+  s.replace("@@TEMP@@", String(temp));
+  s.replace("@@HUM@@", String(hum));
+#endif
   server.send(200, "text/html", s);
 }
 
 } // namespace
+
+// https://github.com/esp8266/Arduino/blob/master/cores/esp8266/TypeConversion.cpp
+const char base36Chars[36] PROGMEM = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+const uint8_t base36CharValues[75] PROGMEM {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0, 0, 0, 0, 0, 0, // 0 to 9
+          10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 0, 0, 0, 0, 0, 0, // Upper case letters
+          10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35  // Lower case letters
+};
+uint8_t *hexStringToUint8Array(const String &hexString, uint8_t *uint8Array, const uint32_t arrayLength) {
+    assert(hexString.length() >= arrayLength * 2); 
+    for (uint32_t i = 0; i < arrayLength; ++i) {
+        uint8Array[i] = (pgm_read_byte(base36CharValues + hexString.charAt(i * 2) - '0') << 4) + pgm_read_byte(base36CharValues + hexString.charAt(i * 2 + 1) - '0');
+    }
+    return uint8Array;
+}
+
+struct MiningJob
+{
+  String last_block_hash;
+  String expected_hash_str; 
+  uint8_t expected_hash[20];
+  unsigned int difficulty;
+
+  bool parse(char* job_str)
+  {
+    String tokens[3];
+    char *token = strtok(job_str, ",");
+    for (int i = 0; token != NULL && i < 3; i++)
+    {
+      tokens[i] = token;
+      token = strtok(NULL, ",");
+    }
+
+    last_block_hash = tokens[0];
+    expected_hash_str = tokens[1];
+    hexStringToUint8Array(expected_hash_str, expected_hash, 20);
+    difficulty = tokens[2].toInt() * 100 + 1;
+
+    return true;
+  }
+};
+
 
 void setup() {
   Serial.begin(115200);
   Serial.println("\nDuino-Coin " + String(MINER_VER));
   pinMode(LED_BUILTIN, OUTPUT);
 
+  #ifdef USE_MQTT
+    mqttClient.setServer(mqtt_server, mqtt_port);
+  #endif
+  
+  #ifdef USE_DHT
+    Serial.println("Initializing DHT sensor");
+    dht.begin();
+    Serial.println("Test reading: " + String(dht.readHumidity()) + "% humidity");
+    Serial.println("Test reading: temperature " + String(dht.readTemperature()) + "*C");
+  #endif
+
   // Autogenerate ID if required
   chipID = String(ESP.getChipId(), HEX);
-  if( strcmp(RIG_IDENTIFIER, "Auto") == 0 ){
+  
+  if(strcmp(RIG_IDENTIFIER, "Auto") == 0 ){
     AutoRigName = "ESP8266-" + chipID;
     AutoRigName.toUpperCase();
     RIG_IDENTIFIER = AutoRigName.c_str();
@@ -508,8 +653,8 @@ void setup() {
 
   lwdtFeed();
   lwdTimer.attach_ms(LWD_TIMEOUT, lwdtcb);
-  if (USE_HIGHER_DIFF) START_DIFF = "ESP8266H";
-  else START_DIFF = "ESP8266";
+  if (USE_HIGHER_DIFF) START_DIFF = "ESP8266NH";
+  else START_DIFF = "ESP8266N";
 
   if(WEB_DASHBOARD) {
     if (!MDNS.begin(RIG_IDENTIFIER)) {
@@ -543,40 +688,78 @@ void loop() {
   if(WEB_DASHBOARD) server.handleClient();
 
   ConnectToServer();
-  Serial.println("Asking for a new job for user: " + String(USERNAME));
-  client.print("JOB," + String(USERNAME) + "," + String(START_DIFF) + "," + String(MINER_KEY));
+  Serial.println("Asking for a new job for user: " + String(DUCO_USER));
+
+  #ifndef USE_DHT
+    client.print("JOB," + 
+                 String(DUCO_USER) + SEP_TOKEN +
+                 String(START_DIFF) + SEP_TOKEN +
+                 String(MINER_KEY) + END_TOKEN);
+  #endif
+
+  #ifdef USE_DHT
+    temp = dht.readTemperature();
+    hum = dht.readHumidity();
+
+    Serial.println("DHT readings: " + String(temp) + "*C, " + String(hum) + "%");
+    client.print("JOB," + 
+                 String(DUCO_USER) + SEP_TOKEN +
+                 String(START_DIFF) + SEP_TOKEN +
+                 String(MINER_KEY) + SEP_TOKEN +
+                 String(temp) + "@" + String(hum) + END_TOKEN);
+  #endif
+  
+  #ifdef USE_MQTT
+  
+  if (!mqttClient.connected()) {
+    mqttReconnect();
+  }
+  mqttClient.loop();
+    #ifdef USE_DHT
+    long now = millis();
+    if (now - lastMsg > mqtt_update_time) {
+      lastMsg = now;
+      mqttClient.publish(temperature_topic, String(temp).c_str(), true);
+      mqttClient.publish(humidity_topic, String(hum).c_str(), true); 
+    }
+    #endif
+
+  #endif
 
   waitForClientData();
-  String last_block_hash = getValue(client_buffer, SEP_TOKEN, 0);
-  String expected_hash = getValue(client_buffer, SEP_TOKEN, 1);
-  difficulty = getValue(client_buffer, SEP_TOKEN, 2).toInt() * 100 + 1;
+  Serial.println("Received job with size of " + String(client_buffer));
 
-  int job_len = last_block_hash.length() + expected_hash.length() + String(difficulty).length();
-  Serial.println("Received a correct job with size of " + String(job_len) + " bytes");
-  expected_hash.toUpperCase();
+  MiningJob job;
+  job.parse((char*)client_buffer.c_str());
+  difficulty = job.difficulty;
+
+  Serial.println("Parsed job: " + job.last_block_hash + " " + job.expected_hash_str + " " + String(job.difficulty));
+
+  if (USE_HIGHER_DIFF) system_update_cpu_freq(160);
+
   br_sha1_init(&sha1_ctx_base);
-  br_sha1_update(&sha1_ctx_base, last_block_hash.c_str(), last_block_hash.length());
+  br_sha1_update(&sha1_ctx_base, job.last_block_hash.c_str(), job.last_block_hash.length());
 
   float start_time = micros();
   max_micros_elapsed(start_time, 0);
 
   String result = "";
-  digitalWrite(LED_BUILTIN, HIGH);
-  for (unsigned int duco_numeric_result = 0; duco_numeric_result < difficulty; duco_numeric_result++) {
+  if (LED_BLINKING) digitalWrite(LED_BUILTIN, LOW);
+  for (unsigned int duco_numeric_result = 0; duco_numeric_result < job.difficulty; duco_numeric_result++) {
     // Difficulty loop
     sha1_ctx = sha1_ctx_base;
     duco_numeric_result_str = String(duco_numeric_result);
+
     br_sha1_update(&sha1_ctx, duco_numeric_result_str.c_str(), duco_numeric_result_str.length());
     br_sha1_out(&sha1_ctx, hashArray);
-    result = experimental::TypeConversion::uint8ArrayToHexString(hashArray, 20);
-    if (result == expected_hash) {
+
+    if (memcmp(job.expected_hash, hashArray, 20) == 0) {
       // If result is found
+      if (LED_BLINKING) digitalWrite(LED_BUILTIN, HIGH);
       unsigned long elapsed_time = micros() - start_time;
       float elapsed_time_s = elapsed_time * .000001f;
       hashrate = duco_numeric_result / elapsed_time_s;
       share_count++;
-      blink(BLINK_SHARE_FOUND);
-
       client.print(String(duco_numeric_result)
                    + ","
                    + String(hashrate)
@@ -587,7 +770,8 @@ void loop() {
                    + ","
                    + String(RIG_IDENTIFIER)
                    + ",DUCOID"
-                   + String(chipID));
+                   + String(chipID)
+                   + "\n");
 
       waitForClientData();
       Serial.println(client_buffer
@@ -601,13 +785,8 @@ void loop() {
                      + "s)");
       break;
     }
-    if (max_micros_elapsed(micros(), 500000))
-    {
+    if (max_micros_elapsed(micros(), 500000)) {
       handleSystemEvents();
-    }
-    else
-    {
-      delay(0);
     }
   }
 }
